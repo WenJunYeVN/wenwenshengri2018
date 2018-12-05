9c05aebdcece4e5fbe6af88dc0e17cac2ec5adc5
WenJunYeVN <wenjunye.vietnam@gmail.com>
2018-12-05 19:10:39 +0700

    Update site.js

--- asset/js/site.js
+++ asset/js/site.js
@@ -1,4 +1,4 @@
-$(window).load(function() {
+$(window).on('load', function() {
   // Animate loader off screen
   $(".page-loader").fadeOut("slow");
 
@@ -19,27 +19,6 @@ $(window).load(function() {
 
 $(document).ready(function(){
   $('.hpbd-text-2, .giftbox').hide();
-
-  /*var loader = $(".page-loader");
-
-  if (loader.data("active")) { return; }
-  loader.show().data("active", true);
-  setTimeout(function() {
-    loader.fadeOut().data("active", false);
-    $('.hpbd-text-1 > .tlt').textillate();
-
-    setTimeout(function() {
-      $('.hpbd-text-1').fadeOut();
-      $('.hpbd-text-2').fadeIn().addClass('animated tada');
-
-      setTimeout(function() {
-        $('.giftbox').fadeIn().addClass('animated bounce infinite');
-        $.scrollify({
-          section : ".section",
-        });
-      }, 3000);
-    }, 3000);
-  }, 3000);*/
 
   $('#firstLetter').on('click', function(e) {
     e.preventDefault();
